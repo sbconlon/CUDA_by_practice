a1083c64d888f4486c920c4a66c7f780d4e62097
sbc98@hotmail.com <sean@LAPTOP-60GQIC2L.localdomain>
2021-07-11 16:12:05 -0700

    finished 01

--- 01_Background/Source_code/00add_P.cu
+++ 01_Background/Source_code/00add_P.cu
@@ -4,6 +4,9 @@
 // Do the add vector operation
 int* add(int* a, int* b, int* result, int N) {
     // -:YOUR CODE HERE:-
+    for(size_t i=0; i<N; i++)
+        result[i] = a[i] + b[i];
+    return result;
 }
 
 // Configure the dynamic memory to initialize
@@ -13,6 +16,9 @@ void onHost() {
     int N = 10;
 
     // -:YOUR CODE HERE:-
+    int* a = (int*) malloc(N*sizeof(int));
+    int* b = (int*) malloc(N*sizeof(int));
+    int* result = (int*) malloc(N*sizeof(int));
 
     for (int i = 0; i < N; i++) {
         a[i] = -i;
