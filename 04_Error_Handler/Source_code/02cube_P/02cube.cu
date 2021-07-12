07cfd9e7af5c87e95fceb32d15ba25e258a40660
Sean Conlon <conlon@berkeley.edu>
2021-07-11 22:34:02 -0700

    finished 04

--- 04_Error_Handler/Source_code/02cube_P/02cube.cu
+++ 04_Error_Handler/Source_code/02cube_P/02cube.cu
@@ -4,34 +4,44 @@
 
 __global__ void cubeKernel(float* d_out, float* d_in) {
     // -:YOUR CODE HERE:-
+    size_t tid = blockDim.x * blockIdx.x + threadIdx.x;
+    d_out[tid] = d_in[tid] * d_in[tid] * d_in[tid];
 }
 
 void onDevice(float* h_in, float* h_out, int ARRAY_SIZE, int ARRAY_BYTES) {
     // declare GPU memory pointers
     // -:YOUR CODE HERE:-
+    float *d_in, *d_out;
 
     // allocate GPU memory
     // -:YOUR CODE HERE:-
+    HANDLER_ERROR_ERR(cudaMalloc((void**)&d_in, ARRAY_BYTES));
+    HANDLER_ERROR_ERR(cudaMalloc((void**)&d_out, ARRAY_BYTES));
 
     // transfer the array to the GPU
     // -:YOUR CODE HERE:-
+    HANDLER_ERROR_ERR(cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice));
 
     // launch the kernel
     cubeKernel<<<1, ARRAY_SIZE>>>(d_out, d_in);
 
     // copy back the result array to the CPU
     // -:YOUR CODE HERE:-
+    HANDLER_ERROR_ERR(cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost));
 
     // free GPU memory pointers
     // -:YOUR CODE HERE:-
+    HANDLER_ERROR_ERR(cudaFree(d_in));
+    HANDLER_ERROR_ERR(cudaFree(d_out));
 }
 
 void test(float* h_in, float* h_out, int ARRAY_SIZE, int ARRAY_BYTES) {
     // print out the resulting array
     for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d^3 = %f\n", i, h_out[i]);
         assert(h_out[i] == (h_in[i] * h_in[i] * h_in[i]));
-        printf("%f", h_out[i]);
-        printf(((i % 4) != 3) ? "\t" : "\n");
+        //printf("%f", h_out[i]);
+        //printf(((i % 4) != 3) ? "\t" : "\n");
     }
 
     printf("-: successful execution :-\n");
@@ -43,9 +53,13 @@ void onHost() {
 
     // declare CPU memory pointers
     // -:YOUR CODE HERE:-
+    float *h_in, *h_out;
+
 
     // Allocate CPU memory pointers
     // -:YOUR CODE HERE:-
+    h_in = (float*)malloc(ARRAY_BYTES);
+    h_out = (float*)malloc(ARRAY_BYTES);
 
     for (int i = 0; i < ARRAY_SIZE; i++) {
         h_in[i] = float(i);
@@ -57,6 +71,8 @@ void onHost() {
 
     // free CPU memory pointers
     // -:YOUR CODE HERE:-
+    free(h_in);
+    free(h_out);
 }
 
 int main(int argc, char** argv) {
